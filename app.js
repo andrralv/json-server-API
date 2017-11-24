046bc0ee5b55bd5334c946c05a290e4b46346329
ttymarucr <ttymarucr@gmail.com>
2017-11-24 15:56:26 -0600

    Se agrega funcionalidad para invocar contratos

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var web3 = require('web3');
-var truffle = require('truffle-contract');
-
-var Sensor = require('./build/contracts/Sensor.json');
+var TruffleContract = require('truffle-contract');
 
 web3 = new web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+var SensorABI = require('./build/contracts/Sensor.json');
+var contracts = [];
+
+contracts.Sensor = TruffleContract(SensorABI);
+contracts.Sensor.setProvider(web3.currentProvider);
 
 var app = express();
 
@@ -29,22 +33,22 @@ app.post('/', function (req, res) {
     }
     web3.personal.unlockAccount(
         web3.eth.accounts[0],
-        "1234");
-    var tx = web3.eth.sendTransaction(
+        "1234"
+    );
+    contracts.Sensor.deployed().then(instance=>{
+        return instance.putData(Math.floor((Math.random() * 10) + 1), {from: web3.eth.accounts[0]});
+    }).then(result=>{
+        res.json(
             {
-                from: web3.eth.accounts[0],
-                to:"0x0093E74f10660FFb20Adfc336a8aAfd08dD9A28C",
-                value: web3.toWei(1, 'ether'),
-                gasPrice: 20000000000
+                "blockNumber": web3.eth.blockNumber,
+                "result" : result
             }
         );
-    res.json(
-        {
-            "HELLO": req.body.THREE,
-            "blockNumber": web3.eth.blockNumber,
-            "tx": tx
-        }
-    );    
+    }).catch(error => { 
+        console.log('error', error);
+        res.json({"error":error.message});
+    });
+        
 });
 
 module.exports = app;
